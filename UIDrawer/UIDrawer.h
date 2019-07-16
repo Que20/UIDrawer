fe7b10fd8c3d4f305181757597577f1740248980
Kevin Maarek <me@kevinmaarek.fr>
2019-07-16 15:20:31 +0200

    Initial commit.

---
+++ UIDrawer/UIDrawer.h
@@ -0,0 +1,19 @@
+//
+//  UIDrawer.h
+//  UIDrawer
+//
+//  Created by Kevin Maarek on 16/07/2019.
+//  Copyright © 2019 que20. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIDrawer.
+FOUNDATION_EXPORT double UIDrawerVersionNumber;
+
+//! Project version string for UIDrawer.
+FOUNDATION_EXPORT const unsigned char UIDrawerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIDrawer/PublicHeader.h>
+
+
